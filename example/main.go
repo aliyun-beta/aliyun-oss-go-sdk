25fd78b7197b9bf68d334b5d214ea8b1e6b2bac1
Hǎiliàng Wáng <w@h12.me>
2015-11-04 23:32:49 +0800

    update doc

---
+++ example/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+)
+
+func main() {
+	var accessKeyID, accessKeySecret string
+	flag.StringVar(&accessKeyID, "id", "", "access key ID")
+	flag.StringVar(&accessKeySecret, "secret", "", "access key Secret")
+	flag.Parse()
+	if accessKeyID == "" || accessKeySecret == "" {
+		fmt.Println("go run main.go -id <your id> -secret <your secret>")
+		flag.PrintDefaults()
+		return
+	}
+
+	endPoint := "oss-cn-hangzhou.aliyuncs.com"
+
+	// Create an API object
+	api := oss.New(endPoint, accessKeyID, accessKeySecret)
+
+	// List all buckets
+	bucketList, err := api.GetService()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("OK accessing OSS")
+	log.Println(bucketList)
+
+	// Create a bucket
+	if err := api.PutBucket("bucket-name", oss.PrivateACL); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("bucket created or existed")
+
+	// Upload an object
+	testStr := "your test string"
+	if err := api.PutObject("bucket-name", "object/name", strings.NewReader(testStr)); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("object uploaded")
+
+	// Get the object
+	buf := new(bytes.Buffer)
+	headers, err := api.GetObject("bucket-name", "object/name", buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("object downloaded")
+	log.Println(headers)
+
+	if buf.String() != testStr {
+		log.Fatalf("expectd %s but got %s", testStr, buf.String())
+	}
+	log.Println("object contents match")
+}
